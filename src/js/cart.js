5ec796533a2b741dee0649701583598bd36e3413
Santiago Bergerat <santiagobergerat@gmail.com>
2025-09-17 23:44:36 -0300

    feat : added dinamic footer andd header

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,10 +1,10 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage } from './utils.mjs';
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = getLocalStorage('so-cart');
   if (cartItems) {
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-    document.querySelector(".product-list").innerHTML = htmlItems.join("");
+    document.querySelector('.product-list').innerHTML = htmlItems.join('');
   }
 }
 
