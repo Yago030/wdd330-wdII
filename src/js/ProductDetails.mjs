48f10eb2b40b177c7403fea1c6b4629626d5c300
El-Luih <luisalejandro632@gmail.com>
2025-09-20 03:29:29 -0600

    Display for product discount in product details.

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -31,15 +31,31 @@ function productDetailsTemplate(product) {
   document.querySelector("h2").textContent = product.Category.charAt(0).toUpperCase() + product.Category.slice(1);
   document.querySelector("#p-brand").textContent = product.Brand.Name;
   document.querySelector("#p-name").textContent = product.NameWithoutBrand;
+  
 
   const productImage = document.querySelector("#p-image");
   productImage.src = product.Images.PrimaryExtraLarge;
   productImage.alt = product.NameWithoutBrand;
-  const euroPrice = new Intl.NumberFormat('de-DE',
-    {
-      style: 'currency', currency: 'EUR',
-    }).format(Number(product.FinalPrice) * 0.85);
-  document.querySelector("#p-price").textContent = `${euroPrice}`;
+
+  const listPrice = product.ListPrice;
+  const finalPrice = product.FinalPrice;
+  const productPrice = document.querySelector("#p-price");
+  const productDiscount = document.querySelector("#p-discount");
+
+  /* Changed how the price is displayed.
+   If the list price of a product differs from the final price
+   and is also higher, "p-price" will display the list price,
+   "p-discount" will become visible (since it's hidden by default),
+   and it will display the final discounted price as well. */
+
+  if (listPrice != finalPrice && listPrice > finalPrice) {
+    productPrice.innerHTML = `$${listPrice}`;
+    productPrice.classList.add('discount-price');
+    productDiscount.textContent = `  $${finalPrice}`
+    productDiscount.classList.add('show');
+  } else {
+    productPrice.innerHTML = `$${finalPrice}`;
+  }
   document.querySelector("#p-color").textContent = product.Colors[0].ColorName;
   document.querySelector("#p-description").innerHTML = product.DescriptionHtmlSimple;
 
