61e2a4972ce942fd75b41424d125b4750cb287a6
El-Luih <luisalejandro632@gmail.com>
2025-09-20 00:01:24 -0600

    Linting and formatting.

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,9 @@
-import { getParam, loadHeaderFooter } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
-import ProductDetails from "./ProductDetails.mjs";
+import { getParam, loadHeaderFooter } from './utils.mjs';
+import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
 
 const dataSource = new ProductData();
-const productID = getParam("product");
+const productID = getParam('product');
 
 const product = new ProductDetails(productID, dataSource);
 product.init();
